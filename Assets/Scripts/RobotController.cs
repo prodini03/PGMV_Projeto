c6306433fe126a1258c5d787deb2a6b3af6d15f6
Lourenco21 <lcfvs@iscte-iul.pt>
2025-04-24 02:21:41 +0100

    Cinemachine camera setup

--- Assets/Scripts/RobotController.cs
+++ Assets/Scripts/RobotController.cs
@@ -1,28 +1,34 @@
 using UnityEngine;
 
-[RequireComponent(typeof(Rigidbody))]
 public class RobotController : MonoBehaviour
 {
 
+    public float speed = 1000f;
     public float verticalForce = 10f;
-    [SerializeField] float turnRate = 1000f;
-    [SerializeField] float speed = 1000f;
+
+    public float hoverForce = 5f;
+    public float hoverFrequency = 2f;
+
+    public float mouseSensitivity = 2f;
 
     private Rigidbody rb;
+
+    private float horizontalCameraMove;
 
     void Start()
     {
         rb = GetComponent<Rigidbody>();
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
     }
 
     void FixedUpdate()
     {
 
-        float moveInput = Input.GetAxis("Vertical");
-        rb.AddForce(transform.forward * moveInput * speed * Time.deltaTime);
-        float turnInput = Input.GetAxis("Horizontal");
-        rb.AddTorque(Vector3.up * turnInput * turnRate * Time.deltaTime);
-
+        float moveForward = Input.GetAxis("Vertical");
+        float moveRight = Input.GetAxis("Horizontal");
+        Vector3 moveDirection = (transform.forward * moveForward + transform.right * moveRight).normalized;
+        rb.AddForce(moveDirection * speed * Time.deltaTime);
 
         if (Input.GetKey(KeyCode.Space))
         {
@@ -32,5 +38,18 @@ public class RobotController : MonoBehaviour
         {
             rb.AddForce(Vector3.down * verticalForce);
         }
+
+        float hoverOffset = Mathf.Sin(Time.time * hoverFrequency) * hoverForce;
+        rb.AddForce(Vector3.up * hoverOffset * Time.deltaTime, ForceMode.Force);
+    }
+
+    void Update()
+    {
+        float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity;
+
+        horizontalCameraMove += mouseX;
+
+        transform.rotation = Quaternion.Euler(0f, horizontalCameraMove, 0f);
+
     }
 }
